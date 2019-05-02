42f99a0ae1895f92b69d5b7d6aa37dd3845df4d5
ivanmviveros <ivan.viveros@correounivalle.edu.co>
2019-05-02 02:36:06 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ set -e
 
 apt-get update
 apt-get install -y software-properties-common
-apt-get install -y octave
+apt-get install -y octave liboctave-dev
 apt-get remove -y software-properties-common
 
 # cleanup package manager
